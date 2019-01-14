ab146f195de7fd1245399c4c3c87217ce3d2a3ed
Carrie Hunner <clh87@duke.edu>
2019-01-13 21:34:36 -0500

    started plan and created class

---
+++ src/Breakout.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.shape.Line;
+
+
+
+public class Breakout extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String args[]){
+        launch(args);
+    }
+}
