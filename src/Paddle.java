ba2ad0f7db2ef8dd68f37a511cddf7a758a439aa
Carrie Hunner <clh87@duke.edu>
2019-01-15 15:21:16 -0500

    Started Paddle class - purely graphic and outline methods

--- src/Paddle.java
+++ src/Paddle.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 
-public class Paddle {
+public class Paddle extends Rectangle {
+    private int mySpeed = 20;
+
+    private Point myLocation = new Point(180, 390);
+
+    public static final double WIDTH = 40.0;
+    public static final double HEIGHT = 10.0;
+
+    private static final Paint paddleFill = Color.CORAL;
+    private static final Paint paddleStroke = Color.BLACK;
+
+    Paddle(){
+        this.setHeight(HEIGHT);
+        this.setWidth(WIDTH);
+        this.setFill(paddleFill);
+        this.setStroke(paddleStroke);
+    }
+
+    public void setSpeed(int speed){
+        mySpeed = speed;
+    }
+
+    public void setLocation(Point point){
+        myLocation = point;
+    }
 }
