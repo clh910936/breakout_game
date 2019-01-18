ac58694ce708cf0cffec5f0a18d6f4c31150af22
Carrie Hunner <clh87@duke.edu>
2019-01-17 21:39:17 -0500

    Level one scene initializes and updates paddle abilities

--- src/LevelOneScene.java
+++ src/LevelOneScene.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import javafx.scene.Group;
 
-public class LevelOneScene {
+public class LevelOneScene extends LevelScene {
+
+    LevelOneScene(String fileName, Group root) throws Exception {
+        super(fileName, root);
+        initializeLevelOne();
+    }
+
+    private void initializeLevelOne(){
+        turnOffPaddleWallCollide();
+    }
+
+    //Sets paddle to not hit walls - go around the corner
+    private void turnOffPaddleWallCollide(){
+        for(int k = 0; k < myPaddles.size(); k++){
+            myPaddles.get(k).setWallCollideOn(false);
+        }
+    }
 }
