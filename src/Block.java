d7efaac46587b2072a02b94aadb917dd0c7901e0
Carrie Hunner <clh87@duke.edu>
2019-01-26 00:18:08 -0500

    commented Block

--- src/Block.java
+++ src/Block.java
@@ -6,6 +6,8 @@
  *
  * Dependent on Logistics class for logging the score increase that accompanies hitting
  * a block
+ *
+ * @example Block testBlock = new Block(4, point, logistic);
  */
 
 import javafx.scene.paint.Color;
@@ -28,6 +30,12 @@ public class Block extends Rectangle {
     private final Paint FIVE_HEALTH = Color.rgb(200, 0, 0);
     private final Paint OUTLINE = Color.BLACK;
 
+    /**
+     * Initializes the instance variables and sets up the graphics
+     * @param health: int between 1 and 5 for the number of hits to break the block
+     * @param point: Point representing where the upper left corner of the block should be
+     * @param logistic: Logistic to add score to, keep track of levels, and lives
+     */
     Block(int health, Point point, Logistics logistic){
         myHealth = health;
         myX = point.getX();
@@ -35,8 +43,6 @@ public class Block extends Rectangle {
         myLogistic = logistic;
 
         setBlockProperties();
-
-
     }
 
     /*
@@ -44,12 +50,23 @@ public class Block extends Rectangle {
      */
 
     //Updates health and logistics score
+
+    /**
+     * Called when a block is hit.
+     * It decreases the health of the block and increases the score for the player.
+     */
     public void blockHit(){
         myHealth -= 1;
         myLogistic.increaseScore(10);
     }
 
     //Returns true of block is destroyed, false otherwise
+
+    /**
+     * Checks if the block is out of health. If not,
+     * it updates the color of block
+     * @return boolean: true if destroyed, false if not
+     */
     public boolean isBlockDestroyed(){
         if(myHealth != 0){
             this.setFill(determineColor());
