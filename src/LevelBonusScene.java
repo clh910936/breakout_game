1a5a9e8bb3f8bc5df5ef6aee1543fdfd96a2fd46
Carrie Hunner <clh87@duke.edu>
2019-01-26 00:31:28 -0500

    commented LevelBonusScene

--- src/LevelBonusScene.java
+++ src/LevelBonusScene.java
@@ -1,7 +1,7 @@
 /**
  * @author Carrie Hunner
  * This class is an extension of LevelScene
- * It creates a level of randomly generated blocksd
+ * It creates a level of randomly generated blocks
  */
 
 import javafx.scene.Group;
@@ -9,6 +9,12 @@ import java.util.HashSet;
 
 public class LevelBonusScene extends LevelScene {
 
+    /**
+     * Calls the constructor from LevelScene
+     * @param filename: in this case, the filename doesn't matter because it's never used
+     * @param root: root of the program
+     * @param logistic: instance of logistic used by the other classes
+     */
     LevelBonusScene(String filename, Group root, Logistics logistic) throws Exception {
         super(filename, root, logistic);
 
@@ -16,6 +22,11 @@ public class LevelBonusScene extends LevelScene {
 
     //Generates a random number of blocks at random coordinates with random health to create the level
     //Overrides the createLevel() in LevelScene
+
+    /**
+     * Overrides the createLevel() from the LevelScene class and sets up the number of random blocks
+     * to be generated. It then calls a private method to actually create those blocks
+     */
     @Override
     public void createLevel(){
         //numbers chosen so the whole screen can't be too full or too empty
